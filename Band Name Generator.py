833c0c4df90b4668fc46c2dbebe40c43326941d2
vadim <symphatixe@gmail.com>
2023-09-29 07:30:05 -0400

    File directory additions

--- Band Name Generator.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to the brand new Band Name Generator")
-city = input("What city where you born in?\n")
-pet = input("What is the name of your first pet?\n")
-
-print("The name of your new band could be: " + city + " " + pet)
