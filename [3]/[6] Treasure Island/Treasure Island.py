f5631486b22235544dfa88eeaadf255e6b2fde01
vadim <symphatixe@gmail.com>
2023-11-24 14:21:34 -0500

    Update

---
+++ [3]/[6] Treasure Island/Treasure Island.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+choice1 = input("Pick a direction to go, left or right.")
+
+if (choice1.lower() == "left"):
+    choice2 = input("There is a small sewer in front of you, do you swim through it or wait?")
+    
+    if (choice2.lower() == "swim"):
+        print("You are attacked by mutated trout, game over!")
+    else:
+        choice3 = input("There are three doors in front of you, red, blue, and yellow. You can also choose to walk back. Choose between red, blue, yellow, and back.")
+        
+        if (choice3.lower() == "red"):
+            print("You are burned by fire, game over!")
+        elif (choice3.lower() == "blue"):
+            print("You are eaten by monsters, game over!")
+        elif (choice3.lower() == "yellow"):
+            print("You found the treasure!")
+        else:
+            print("You chose to walk back or do something else, a huge rock falls from the top and crushes you, game over!")
+            
+            
+else:
+    print("You fell into a hole, game over!")
---
+++ [4]/CoinToss.py
@@ -0,0 +1,17 @@
+import random
+
+print("Welcome to the coin toss simulator, enter toss and choose a value in your head. See if your prediction is right")
+choice = input("Heads or tails?")
+
+toss = random.randint(0, 1)
+if (toss == 0):
+    toss = str("tails")
+else:
+    toss = str("heads")
+
+if (toss == 0 and choice.lower() == "tails"):
+    print("You chose tails, congratulations!")
+elif (toss == 1 and choice.lower() == "heads"):
+    print("You chose heads, congratulations!")
+else:
+    print(f"Your prediction was not correct, try again. You picked {choice} and the coin was {toss}. 0 means tails and 1 is heads.")
