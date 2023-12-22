b3d3b21e2771f618718a44e7e25a3af514b6d997
vadim <symphatixe@gmail.com>
2023-12-22 15:54:11 -0500

    Updates to Day 4

---
+++ [4]/CoinToss/CoinToss.py
@@ -0,0 +1,4 @@
+import random
+
+print("Welcome to the coin toss simulator, enter toss and choose a value in your head. See if your prediction is right")
+choice = input("Heads or tails?")
---
+++ [4]/Dinner/Dinner.py
@@ -0,0 +1,3 @@
+import random
+
+name_string = input("Enter the names of everyone, separated by a comma.")
