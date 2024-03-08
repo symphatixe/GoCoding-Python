60a27dd5424aea8f34064df9680f37006d14d143
vadim <symphatixe@gmail.com>
2024-03-08 00:53:25 -0500

    Update Coin Toss.py

--- [4]/[1] CoinToss/Coin Toss.py
+++ [4]/[1] CoinToss/Coin Toss.py
@@ -1,7 +1,7 @@
 # Starter Code
 import random
 
-print("Welcome to the coin toss simulator, enter toss and choose a value in your head. See if your prediction is right")
+print("Welcome to the coin toss simulator, enter toss and choose a value in your head. See if your prediction is right!")
 choice = input("Heads or tails? \n")
 
 
