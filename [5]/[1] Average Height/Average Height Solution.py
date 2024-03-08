7e03ce32de88d0c1c51e82e287f7b150797e31dd
vadim <symphatixe@gmail.com>
2024-03-08 00:55:40 -0500

    Update Average Height Solution.py

--- [5]/[1] Average Height/Average Height Solution.py
+++ [5]/[1] Average Height/Average Height Solution.py
@@ -2,7 +2,7 @@
 student_heights = [180, 124, 165, 173, 189, 169, 146]
 
 
-#Write your code below!
+# Write your code below!
 # Important You should not use the sum() or len() functions in your answer.
 
 total = 0
