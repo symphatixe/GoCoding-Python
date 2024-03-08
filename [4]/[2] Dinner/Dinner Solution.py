e616df140b9db4df93c505d46cc4b875f080730b
vadim <symphatixe@gmail.com>
2024-03-08 00:55:00 -0500

    Update Dinner Solution.py

--- [4]/[2] Dinner/Dinner Solution.py
+++ [4]/[2] Dinner/Dinner Solution.py
@@ -1,6 +1,9 @@
+# Starter Code
 import random
 
 name_string = input("Enter the names of everyone, separated by a comma. \n")
 names = name_string.split(", ")
 
+
+# Write your code below!
 print(names[random.randint(0, len(names) - 1)] + " will pay for dinner today!")
