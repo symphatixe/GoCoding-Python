a947d90888cd3089e994709085b04b9e559de3dd
vadim <symphatixe@gmail.com>
2024-03-15 18:08:11 -0400

    Update Positive List Solution.py

--- [5]/[10] Positive List/Positive List Solution.py
+++ [5]/[10] Positive List/Positive List Solution.py
@@ -8,12 +8,9 @@ list2 = []
 # Write your code below!
 for num in range(35):
     number = random.randint(-20, 20)
+    list.append(number)
     if (number >= 0):
-        list.append(number)
         list2.append(number)
-    else:
-        list.append(number)
-        
 
 print("Full list: " + str(list))
 print("Positive list: " + str(list2))
