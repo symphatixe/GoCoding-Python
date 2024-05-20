8e94c84b471f63cdc6871e72eff40287101fcefc
vadim <symphatixe@gmail.com>
2024-05-20 16:25:43 -0400

    Update Hangman III.py

--- [6]/Hangman/Hangman III.py
+++ [6]/Hangman/Hangman III.py
@@ -1,16 +1,84 @@
 import random
 
+stages = [
+'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', 
+'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', 
+'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', 
+'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', 
+'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', 
+'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', 
+'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
+
 print("Welcome to Hangman")
 game_end = False
+lives = 6
 
 word_list = ['baboon', 'books', 'bucket', 'coming', 'camel', 'sunshine']
 choice = random.choice(word_list).lower()
+print(choice)
 
 
 display = []
 for letter in choice:
     display += "_"
-print(display)
 
 
 while not game_end:
@@ -22,8 +90,15 @@ while not game_end:
         if (letter == guess):
             display[position] = guess
             
-    print(display)
+    print(f"{' '.join(display)}\n")
+    
+    if guess not in choice:
+        lives -= 1
+        if lives == 0:
+            game_end = True
+            print("You have lost!")
     
     if ("_") not in display:
         game_end = True
-        print("You have won")
+        print(f"{' '.join(display)}")
+        print(f"You have won, the word is {choice}!")
