9781b86897c12014ba9fb57450befa8a4e4bb98d
vadim <symphatixe@gmail.com>
2024-09-24 19:45:59 -0400

    Update BMI Calculator.py

--- [2] Basic Starter Projects/[2] BMI Calculator/BMI Calculator.py
+++ [2] Basic Starter Projects/[2] BMI Calculator/BMI Calculator.py
@@ -6,5 +6,5 @@ weight = input("enter your weight in kg: \n")
 # Write your code below!
 
 #TODO create two variables that are going to store casted values
-#TODO create a variable to store the height squared for bmi formula: height ^ 2 / weight
+#TODO create a variable to store the height squared for bmi formula: weight / height ^ 2
 #TODO print out the bmi using an f-string, you can create a variable to store it or calculate it inside the f-string
