55d74cb3c64c65937acf7b954f938d07f655b477
vadim <symphatixe@gmail.com>
2024-10-01 16:56:50 -0400

    Update

--- [3] Conditionals/[2] BMI 2.0 Calculator/BMI 2.0.py
+++ [3] Conditionals/[2] BMI 2.0 Calculator/BMI 2.0.py
@@ -5,12 +5,17 @@ weight = float(input("enter your weight in kg: \n"))
 
 # Write your code below!
 
-#TODO copy the code from the BMI calculator and use the height and weight variables insetad of the casted ones
+#TODO copy the code from the BMI calculator and use the height and weight variables instead of the casted ones
 """
 TODO Use conditionals to print out the corresponding text with an f-string based on the BMI:
     - underweight for bmi less than 18.5
     - normal weight for bmi less than 25
     - slightly overweight for bmi less than 30
-    - obesse for bmi less than 35
+    - obese for bmi less than 35
     - everything else is clinically obese    
 """
+height_squared = height**2
+bmi = weight // height_squared
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight")
