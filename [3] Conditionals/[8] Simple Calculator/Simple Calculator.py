428ec29ca8f3e5f94ff4485b718324a5f0f15216
vadim <symphatixe@gmail.com>
2024-10-23 17:42:17 -0400

    Update Simple Calculator.py

--- [3] Conditionals/[8] Simple Calculator/Simple Calculator.py
+++ [3] Conditionals/[8] Simple Calculator/Simple Calculator.py
@@ -7,7 +7,7 @@ operation = int(input("Choose from the menu options below:\n" +
                   "1. Addition\n" +
                   "2. Subtraction\n" +
                   "3. Multiplication\n" +
-                  "4. Division\n"))
+                  "4. Division"))
 
 # Write your code below
 
