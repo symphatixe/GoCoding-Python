0350de9fd0281e9605c37f63f62f9b63088e7f78
vadim <symphatixe@gmail.com>
2025-03-07 16:23:33 -0500

    Update Hangman III.py

--- [6] Functions & Dictionaries/[1] Hangman/Hangman III.py
+++ [6] Functions & Dictionaries/[1] Hangman/Hangman III.py
@@ -76,7 +76,7 @@ word_list = ["baboon", "books", "bucket", "coming", "camel", "sunshine"]
 
 #TODO Create two variables
 # - Boolean to store whether the game has ended
-# - How many lives are left
+# - How many lives are left (7 lives)
 #TODO Create a while loop that will run while game has not ended
 #TODO Inside the while loop we will display the word as underscores to guess
 #TODO The player can guess a letter and will take away lives if the guess is incorrect
