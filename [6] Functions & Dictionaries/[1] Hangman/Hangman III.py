44362ddb446b8254f62afb085db8db932c22ab71
vadim <symphatixe@gmail.com>
2025-03-22 01:11:39 -0400

    Update Hangman III.py

--- [6] Functions & Dictionaries/[1] Hangman/Hangman III.py
+++ [6] Functions & Dictionaries/[1] Hangman/Hangman III.py
@@ -81,21 +81,3 @@ word_list = ["baboon", "books", "bucket", "coming", "camel", "sunshine"]
 #TODO Inside the while loop we will display the word as underscores to guess
 #TODO The player can guess a letter and will take away lives if the guess is incorrect
 #TODO If there are no underscores left then the player has won.
-
-lives = 7
-game_end = False
-
-word = random.choice(word_list).lower()
-display = []
-for _ in word:
-    display += "_"
-
-while not game_end:
-
-  print(f"The word to guess is {" ".join(display)}\n")
-  guess = input("Guess a letter\n").lower()
-
-  for position in range(len(word)):
-      letter = word[position]
-      if (letter == guess):
-          display[position] = guess
